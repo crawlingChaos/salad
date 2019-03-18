b0b0c9d5492287a3e75f6c7e119f6c6f345a3adb
crawlingChaos <11152023+crawlingChaos@users.noreply.github.com>
2019-03-18 17:42:10 -0400

    Added human readable error messages. Speed up generation by collecting all words on a single scan through the word list, rather than one at a time. Added LICENSE and README files. Added default word list.

---
+++ README.md
@@ -0,0 +1,31 @@
+#  Salad
+
+Salad is a random passphrase generator written in Rust for *nix systems. It is Free Software
+and released under the [GNU Affero General Public License V3](http://www.gnu.org/licenses/agpl.html).
+
+Salad's goal is to provide a robust passphrase generator to users who want strong security. It is intended to provide flexible passphrase generation without requiring multiple specialized word lists by dynamically choosing words that match any desired criteria. It will also provide an estimate of the amount of entropy in a given passphrase, controlled by the word list and the passphrase settings.
+
+
+## Installation
+
+Salad is still in a very early state. As such there is no installer or distro specific packages. Compile the code with rustc and place the binary in your PATH, or simply call it from wherever you like. 
+
+
+## Dependencies
+
+Just the Rust standard library and a newline delimited text file to use as a word list. Salad comes with a large word list, although the words were chosen to maximize entropy rather than be particularly easy to memorize or spell. Using a custom word list with Salad is easy.
+
+
+## Usage
+
+Currently Salad will accept the name of a word list file on the command line. Otherwise it will look in **$HOME/.salad/words** first and **/etc/salad/words** second. 
+
+
+## Configuration
+
+Soon Salad will offer user selectable settings such as number of words in the passphrase, and minimum and maximum acceptable word length. Future plans are for more robust filtering based on punctuation, numbers, and perhaps more complex features. Currently settings are defaulted to 6 words with a minimum of 5 and maximum of 10 charatcers per word. When settings are available, they will be able to be set via commandline, environment variables and both system and user prefs files.
+
+
+##Limitations
+
+Currently there are no user selectable settings other than choosing the word list file via command line argument.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{
     env,
     fs::File,
-    io::{BufRead, BufReader, Error, ErrorKind, Read, Result, Seek, SeekFrom},
+    io::{BufRead, BufReader, Read, Seek, SeekFrom},
+    result::Result,
 };
 
 const PROGRAM_NAME: &'static str = "salad";
@@ -16,87 +17,169 @@ fn get_buffer() -> [u8; 4] {
     [0, 0, 0, 0]
 }
 
-fn get_count(words: &mut File, min: usize, max: usize) -> Result<usize> {
-    words.seek(SeekFrom::Start(0))?;
+struct Prefs {
+    min: u8,
+    max: u8,
+    count: u8,
+    words: File,
+    stats: bool,
+    generate: bool,
+}
+
+fn main() {
+    std::process::exit(match run() {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            1
+        }
+    });
+}
+
+// Get a count of words matching the min and max criteria.
+fn get_count(words: &mut File, min: u8, max: u8) -> Result<usize, &'static str> {
+    let min = min as usize;
+    let max = max as usize;
+    seek_start(words)?;
     let mut count = 0;
     for line in BufReader::new(words).lines() {
-        let line_len = line?.len();
-        if line_len > 0 && line_len >= min && line_len <= max {
-            count += 1;
+        match line {
+            Ok(line) => {
+                let len = line.len();
+                if len > 0 && len >= min && len <= max {
+                    count += 1;
+                }
+            }
+            Err(_) => return Err("Error reading from word file"),
         }
     }
     if count > 0 {
         Ok(count)
     } else {
-        Err(Error::new(ErrorKind::Other, "No valid words found."))
+        Err("No valid words found")
     }
 }
 
-fn get_random(max: usize) -> Result<usize> {
+fn get_prefs() -> Result<Prefs, &'static str> {
+    let words = get_word_file()?;
+    Ok(Prefs {
+        min: 5,
+        max: 10,
+        count: 6,
+        words,
+        stats: true,
+        generate: true,
+    })
+}
+
+// Get count random numbers between 0 and max, inclusive.
+fn get_random(count: u8, max: usize) -> Result<Vec<usize>, &'static str> {
     let unused_bits = max.leading_zeros();
     let mask = usize::max_value().wrapping_shr(unused_bits);
     let mut buffer = get_buffer();
     let needed_bytes = buffer.len() - (unused_bits as usize) / 8;
-    let mut random = File::open("/dev/urandom")?;
-    loop {
-        let bytes_read = random.read(&mut buffer[0..needed_bytes])?;
-        if bytes_read < needed_bytes {
-            return Err(Error::new(
-                ErrorKind::Other,
-                "Insufficient random bytes read.",
-            ));
-        }
-        let n = usize::from_le_bytes(buffer) & mask;
-        if n <= max {
-            return Ok(n);
-        }
-    }
-}
-
-fn get_word(words: &mut File, index: usize, min: usize, max: usize) -> Result<String> {
-    words.seek(SeekFrom::Start(0))?;
-    let mut count = 0;
-    for line in BufReader::new(words).lines() {
-        let line = line?;
-        let line_len = line.len();
-        if line_len > 0 && line_len >= min && line_len <= max {
-            if count == index {
-                return Ok(line);
+    match File::open("/dev/urandom") {
+        Ok(mut random) => {
+            let mut results = Vec::new();
+            while results.len() < count as usize {
+                match random.read(&mut buffer[0..needed_bytes]) {
+                    Ok(bytes_read) => {
+                        if bytes_read < needed_bytes {
+                            return Err("Insufficient random bytes read.");
+                        }
+                        let n = usize::from_le_bytes(buffer) & mask;
+                        if n <= max {
+                            results.push(n);
+                        }
+                    }
+                    Err(_) => return Err("Unable to read from /dev/urandom"),
+                }
             }
-            count += 1;
+            Ok(results)
         }
+        Err(_) => return Err("Unable to open /dev/urandom"),
     }
-    Err(Error::new(ErrorKind::Other, "Unexpected end of word list."))
 }
 
-fn get_words() -> Result<File> {
+fn get_word_file() -> Result<File, &'static str> {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        return File::open(&args[1]);
-    }
-    if let Ok(home) = env::var("HOME") {
-        if let Ok(file) = File::open(home + "/." + PROGRAM_NAME + "/words") {
-            return Ok(file);
+        match File::open(&args[1]) {
+            Ok(file) => Ok(file),
+            Err(_) => Err("Unable to open word list"),
         }
-    };
-    File::open(String::from("/etc/") + PROGRAM_NAME + "/words")
+    } else {
+        if let Ok(home) = env::var("HOME") {
+            if let Ok(file) = File::open(home + "/." + PROGRAM_NAME + "/words") {
+                return Ok(file);
+            }
+        }
+        match File::open(String::from("/etc/") + PROGRAM_NAME + "/words") {
+            Ok(file) => Ok(file),
+            Err(_) => Err("Unable to open word list"),
+        }
+    }
 }
 
-fn main() -> std::io::Result<()> {
-    let min = 5;
-    let max = 10;
-    let passphrase_len = 6;
-    let mut words = get_words()?;
-    let word_count = get_count(&mut words, min, max)?;
-    println!("Word count: {}", word_count);
-    println!(
-        "Entropy: {}",
-        f64::from(passphrase_len) * f64::from(word_count as u32).log2()
-    );
-    for _ in 0..passphrase_len {
-        let index = get_random(word_count - 1)?;
-        let word = get_word(&mut words, index, min, max)?;
-        println!("{} ", word);
+// Get the words at the specified indices, ignoring words which do not match the min and max criteria.
+fn get_words(
+    indices: Vec<usize>,
+    words: &mut File,
+    min: u8,
+    max: u8,
+) -> Result<Vec<String>, &'static str> {
+    let mut results = vec!["".to_string(); indices.len()];
+    let min = min as usize;
+    let max = max as usize;
+    seek_start(words)?;
+    let mut count = 0;
+    let mut found = 0;
+    for line in BufReader::new(words).lines() {
+        match line {
+            Ok(line) => {
+                let len = line.len();
+                if len > 0 && len >= min && len <= max {
+                    for (i, &index) in indices.iter().enumerate() {
+                        if count == index {
+                            results[i] = line.clone();
+                            found += 1;
+                            if found == indices.len() {
+                                return Ok(results);
+                            }
+                        }
+                    }
+                    count += 1;
+                }
+            }
+            Err(_) => return Err("Error reading from word file"),
+        }
+    }
+    Err("Unexpected end of word list")
+}
+
+fn run() -> Result<(), &'static str> {
+    let mut prefs = get_prefs()?;
+    let word_count = get_count(&mut prefs.words, prefs.min, prefs.max)?;
+    if prefs.stats {
+        println!("Word count: {}", word_count);
+        println!(
+            "Entropy: {}",
+            f64::from(prefs.count) * f64::from(word_count as u32).log2()
+        );
+    }
+    if prefs.generate {
+        let indices = get_random(prefs.count, word_count - 1)?;
+        let words = get_words(indices, &mut prefs.words, prefs.min, prefs.max)?;
+        for word in words {
+            println!("{} ", word);
+        }
     }
     Ok(())
+}
+
+fn seek_start(file: &mut File) -> Result<(), &'static str> {
+    match file.seek(SeekFrom::Start(0)) {
+        Ok(_) => Ok(()),
+        Err(_) => return Err("Error seeking to start of word file"),
+    }
 }
